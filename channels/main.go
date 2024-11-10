cb9e5a1d3c9448b4d4d6822df13dc03be43a14f0
alikareem123 <55332168+alikareem123@users.noreply.github.com>
2024-11-10 23:07:29 +0530

    interface and channels concurrency part 2

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,18 +14,23 @@ func main() {
 	}
 	c := make(chan string)
 	for _, link := range links {
-		go getLink(link, c)
+		go checkLink(link, c)
 	}
-	fmt.Println(<-c)
+	for l := range c {
+		go func (link string)  {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
 }
 
-func getLink(link string, c chan string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + "might be down")
-		c <- "Might be down i think"
+		c <- link
 		return
 	}
 	fmt.Println(link + " is up!")
-	c <- "It's up"
+	c <- link
 }
